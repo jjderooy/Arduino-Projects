624c70706ea7acabb663df6d930aa61ae326986c
JJ De Rooy <jjderooy123@gmail.com>
2021-07-29 20:51:19 -0700

    Adds sample button code

---
+++ chessboard/button_LED_sample/button_LED_sample.ino
@@ -0,0 +1,51 @@
+
+// Map 0-7 to the pin names
+int btn_indices[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+
+// Return the pin number of any pressed button
+// Returns -1 if no buttons are pressed 
+int get_btn(){
+  for(int i = 0; i < 7; i++){
+    if(digitalRead(btn_indices[i]) == LOW)
+      return btn_indices[i];
+  }
+
+  // No buttons pressed
+  return -1;
+}
+
+// Sets the state of every button IO pin to INPUT_PULLUP or OUTPUT
+// 1 denotes INPUT_PULLUP, 0, denotes OUTPUT
+void set_PINMODE(bool pinmode){
+
+  // INPUT_PULLUP
+  if(pinmode){
+    for(int i = 0; i < 8; i++){
+      pinMode(btn_indices[i], INPUT_PULLUP);
+    }
+  }
+
+  // OUTPUT
+  else{
+    for(int i = 0; i < 8; i++){
+      pinMode(btn_indices[i], OUTPUT);
+    }
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  set_PINMODE(1);
+
+//  for(int i = 0; i < 8; i++){
+//    digitalWrite(btn_indices[i], LOW);
+//    delay(50);
+//    digitalWrite(btn_indices[i], HIGH);
+//    delay(50);
+//  }
+}
